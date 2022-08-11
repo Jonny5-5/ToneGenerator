77d01757a207630d54a4b24faa626717d348a906
Jonny5-5 <35155421+Jonny5-5@users.noreply.github.com>
2022-08-10 22:27:25 -0600

    First commit from laptop

--- app/src/main/java/com/jon/tonegenerator/ToneGenerator.java
+++ app/src/main/java/com/jon/tonegenerator/ToneGenerator.java
@@ -3,36 +3,45 @@ package com.jon.tonegenerator;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.util.Log;
+
+// TODO: Make a circular array for storing / calculating the data or...
+// TODO: I can just keep track of what index I'm on and then calculate each value on-the-fly
+// TODO: When it reaches MAX_INDEX (some multiple of 2 pi) then I set it equal to 0 again
+
 
 public class ToneGenerator {
 
-    private int sampleRate = 16000;
-    private int numSamples = 16000;
-    private double sample[] = new double[numSamples];
-    private final byte soundBuffer[] = new byte[2 * numSamples];
+    final String TAG = "TONE GENERATOR";
+    private int SAMPLE_RATE = 16000;
+    private int NUM_SAMPLES = 16000;
+    private double SAMPLES[] = new double[NUM_SAMPLES];
+    private final byte SOUND_BUFFER[] = new byte[2 * NUM_SAMPLES];
     private double soundFrequency = 0;
     private boolean isPlaying = false, isWriting = false;
     private final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-            sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-            AudioFormat.ENCODING_PCM_16BIT, sampleRate,
+            SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT, SAMPLE_RATE,
             AudioTrack.MODE_STREAM);
 
-    private double phaseShift = 0;
     private void generateTone(){
-        int i;
-        for (i = 0; i < numSamples; ++i) {
-            sample[i] = Math.sin(2.0 * Math.PI * i / ( (double)sampleRate / soundFrequency) + phaseShift);
+
+        if (soundFrequency == 0){
+            Log.d(TAG, "The soundFrequency is 0. Set the freq");
+            return;
         }
-        phaseShift = 2.0 * Math.PI * (i) / ( (double)sampleRate / soundFrequency) + phaseShift;
+        for (int i = 0; i < NUM_SAMPLES; ++i) {
+            SAMPLES[i] = Math.sin(2.0 * Math.PI * i / ((double) SAMPLE_RATE / soundFrequency));
+        }
         // convert to 16 bit pcm sound array
         // assumes the sample buffer is normalized.
         int idx = 0;
-        for (final double dVal : sample) {
+        for (final double dVal : SAMPLES) {
             // scale to maximum amplitude
-            final short val = (short) ((dVal * 32767));
+            final short val = (short) ((dVal * Short.MAX_VALUE));
             // in 16 bit wav PCM, first byte is the low order byte
-            soundBuffer[idx++] = (byte) (val & 0x00ff);
-            soundBuffer[idx++] = (byte) ((val & 0xff00) >>> 8);
+            SOUND_BUFFER[idx++] = (byte) (val & 0x00ff);
+            SOUND_BUFFER[idx++] = (byte) ((val & 0xff00) >>> 8);
         }
     }
 
@@ -48,7 +57,8 @@ public class ToneGenerator {
                 generateTone();
                 while (isPlaying && !isWriting){
                     isWriting = true;
-                    audioTrack.write(soundBuffer, 0, soundBuffer.length);
+                    // TODO: Use the NON_BLOCKING method in the future
+                    audioTrack.write(SOUND_BUFFER, 0, SOUND_BUFFER.length);
                     isWriting = false;
                 }
                 audioTrack.pause();
